c770b3b9620db15f8350639a774c72db53c18d76
Jaakul <jackw1689@gmail.com>
2022-08-23 19:04:09 -0700

    asdf

--- javascript.js
+++ javascript.js
@@ -1,2 +1,16 @@
-let message = "hello, world";
-console.log(message);
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  };
+
+  function multiplyNumeric(menu){
+    for (let key in menu){
+if (typeof (menu[key]) == 'number'){
+    menu[key] *= 2
+}
+
+    }
+  }
+multiplyNumeric(menu);
+  console.log(menu);
